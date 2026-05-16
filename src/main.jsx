e61689f92a738634ae664356e0be7068c3ee2fb1
Rafael Lucero <rafaellucero998@gmail.com>
2026-05-15 23:33:03 -0400

    fix vercel auth

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import './styles/globals.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
-import { initAppCheck } from './firebase/appCheck'
-
-initAppCheck()
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
